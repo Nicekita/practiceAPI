b57b0e5a574b7726f9c07470874c70881669b04e
Nicekita <Nicekita100@gmail.com>
2020-05-18 19:48:45 +0300

    Added auth

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,18 +10,5 @@ import '../css/app.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
-function hello(){
-        $.postJSON('http://projectpractice/Api/public/',{ login: "user1", password: "2pm" }, function(data)
-        {
-  var items = [];
-  $.each(data, function(key, val,json){
-    items.push('<li id="' + key + '">' + val.login + '  ' + val.password + '</li>');
-  });
- 
-  $('<ul/>', {
-    'class': 'my-new-list',
-    html: items.join('')
-  }).appendTo('body');
-})
- };
+
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
--- src/Controller/Sql.php
+++ src/Controller/Sql.php
@@ -10,23 +10,32 @@ class Sql
 {
     function select()
 {
-    $dbconn = pg_connect("host=localhost port=5432 dbname=Kurs user=postgres password=135713")
-    or die('Не удалось соединиться: ' . pg_last_error());;
+    //if (isset($_POST['login'])) {
+        header('Access-Control-Allow-Origin: *');
+        $dbconn = pg_connect("host=localhost port=5432 dbname=AMWbase user=postgres password=135713")
+        or die('Не удалось соединиться: ' . pg_last_error());;
 // Выполнение SQL-запроса
+        $login['auth'] = 0;
         $query = 'SELECT * FROM users';
         $result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
-
         $int = 0;
 // Вывод результатов в HTML
         while ($stroka = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-            $int = $int + 1;
-            $line['user'.$int]=$stroka;
+
+            if ($stroka['login'] == $_POST['login']&& password_verify(htmlspecialchars($_POST['password']), $stroka['pass']) )
+            {
+                $login['auth'] = 1;
+            }
+
         }
 // Очистка результата
 
 
 // Закрытие соединения
         pg_close($dbconn);
-        return new Response(json_encode($line));
-}
+
+        return new Response(json_encode($login));
+    }
+
+//}
 }
